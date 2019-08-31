e2b54fac4dcb85160e9079d423f77b8e0f94ab6d
David Berning <david@oodle.io>
2019-08-31 00:05:26 -0400

    favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   mode: 'universal',
   head: {
-    title: process.env.npm_package_name || '',
+    title: `Dave Berning | Web Developer | Cincinnati, OH`,
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
