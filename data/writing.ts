4e4dd92e0196b9e76d9e289b54fa6ca3c27b3c08
David Berning <david@oodle.io>
2019-09-03 21:58:56 -0400

    new blog post

--- data/writing.ts
+++ data/writing.ts
@@ -1,4 +1,4 @@
-import {Writing} from '@/types';
+import {Writing} from '@/types'
 
 export const writing: Writing[] = [
   {
@@ -6,69 +6,76 @@ export const writing: Writing[] = [
     date: `October 2018`,
     featured: true,
     type: `Book`,
-    to: `https://www.amazon.com/Vue-Js-Understanding-Its-Tools-Ecosystem/dp/9388425022`,
+    to: `https://www.amazon.com/Vue-Js-Understanding-Its-Tools-Ecosystem/dp/9388425022`
   },
   {
     title: `Creating Interfaces With Bulma`,
     date: `February 2018`,
     featured: true,
     type: `Book`,
-    to: `https://www.amazon.com/Creating-Interfaces-Bulma-Jeremy-Thomas-ebook/dp/B079M1BJG4/ref=sr_1_1?keywords=creating+interfaces+with+bulma&qid=1566873504&s=books&sr=1-1`,
+    to: `https://www.amazon.com/Creating-Interfaces-Bulma-Jeremy-Thomas-ebook/dp/B079M1BJG4/ref=sr_1_1?keywords=creating+interfaces+with+bulma&qid=1566873504&s=books&sr=1-1`
+  },
+  {
+    title: `Using TypeScript with Vue Single File Components`,
+    date: `September 3, 2019`,
+    featured: false,
+    type: `Alligator.io`,
+    to: `https://alligator.io/vuejs/using-typescript-with-vue/`
   },
   {
     title: `Getting Started with Server-Side Rendering Using Nuxt.js`,
     date: `April 16, 2018`,
     featured: false,
     type: `Alligator.io`,
-    to: `https://alligator.io/vuejs/server-side-rendering-with-nuxtjs/`,
+    to: `https://alligator.io/vuejs/server-side-rendering-with-nuxtjs/`
   },
   {
     title: `Implementing an Infinite Scroll with Vue.js`,
     date: `April 3, 2018`,
     featured: false,
     type: `Alligator.io`,
-    to: `https://alligator.io/vuejs/implementing-infinite-scroll/`,
+    to: `https://alligator.io/vuejs/implementing-infinite-scroll/`
   },
   {
     title: `Integrating and Using CSS Frameworks with Vue.js`,
     date: `April 1, 2018`,
     featured: false,
     type: `Alligator.io`,
-    to: `https://alligator.io/vuejs/css-frameworks-vuejs/`,
+    to: `https://alligator.io/vuejs/css-frameworks-vuejs/`
   },
   {
     title: `Using Chart.js with Vue.js`,
     date: `January 28, 2018`,
     featured: false,
     type: `Alligator.io`,
-    to: `https://alligator.io/vuejs/vue-chart-js/`,
+    to: `https://alligator.io/vuejs/vue-chart-js/`
   },
   {
     title: `Importing Global Libraries into Vue CLI (The Clean Way)`,
     date: `January 5, 2018`,
     featured: false,
     type: `Frontend Weekly`,
-    to: `https://medium.com/front-end-weekly/importing-global-libraries-into-vue-cli-the-clean-way-9a7945213fa0`,
+    to: `https://medium.com/front-end-weekly/importing-global-libraries-into-vue-cli-the-clean-way-9a7945213fa0`
   },
   {
     title: `Creating Alias’ For Package Imports in React`,
     date: `December 28, 2017`,
     featured: false,
     type: `Frontend Weekly`,
-    to: `https://medium.com/front-end-weekly/creating-alias-for-package-imports-in-react-99d455284029`,
+    to: `https://medium.com/front-end-weekly/creating-alias-for-package-imports-in-react-99d455284029`
   },
   {
     title: `How to Stay Ahead of Google’s Non-Intrusive Popup Update`,
     date: `September 18, 2016`,
     featured: false,
     type: `Medium`,
-    to: `https://medium.com/@daveberning/how-to-stay-ahead-of-googles-non-intrusive-popup-update-6c386f1ac1f6`,
+    to: `https://medium.com/@daveberning/how-to-stay-ahead-of-googles-non-intrusive-popup-update-6c386f1ac1f6`
   },
   {
     title: `Which is Better for SEO?: WWW or non-WWW`,
     date: `September 8, 2016`,
     featured: false,
     type: `Medium`,
-    to: `https://medium.com/@daveberning/which-is-better-for-seo-www-or-non-www-871edd4fe5aa`,
-  },
-];
+    to: `https://medium.com/@daveberning/which-is-better-for-seo-www-or-non-www-871edd4fe5aa`
+  }
+]
