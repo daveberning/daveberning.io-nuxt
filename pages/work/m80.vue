cef401d857a37625995b8043ce3bc816a2162f1c
Dave Berning <daveberning@icloud.com>
2020-02-01 13:01:54 -0500

    m80 project page

--- pages/work/m80.vue
+++ pages/work/m80.vue
@@ -3,8 +3,12 @@
     <work-content title="M80, Inc." url="http://m80inc.com/">
       <template slot="default">
         <img class="img-responsive" src="~/assets/images/m80inc.png" alt="">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod vestibulum molestie. In blandit euismod neque, non pharetra lectus vestibulum eu. Aliquam vel eleifend quam. Quisque consectetur ligula eget neque eleifend, vel mollis arcu ultrices. Curabitur gravida mi id sagittis varius. In ligula magna, fermentum vel venenatis at, mollis cursus augue. Ut faucibus malesuada libero, non ornare quam. Donec vitae vehicula erat, in convallis dolor. Nam in sodales quam, sit amet porttitor metus. Aenean egestas lorem tellus, non congue neque porta sed. Fusce ac ornare metus. Mauris vulputate ipsum at ipsum convallis, et pharetra purus tincidunt. Nam vel ullamcorper orci, id malesuada libero. Suspendisse tincidunt lorem arcu, sit amet scelerisque nibh dictum condimentum. Cras finibus placerat nisl et pellentesque. Mauris urna lectus, placerat ut varius quis, scelerisque ut nulla. </p>
-        </div>
+        <p>M80, Inc is a product design company located in Cincinnati, OH. They are known for breaking the status quo
+          and working in unconventional ways to develop the best product for their clients. Some of their product design work includes: Gorilla Shipping Tape, Tic-Tac Chill, Chapstick Duo, and Barilla Sauce Jar.</p>
+
+        <p>In the spring of 2016, I was approached by M80 designer, Brett Valls, to consult with and develop the new and improved company website. Brett is a graphic and web designer who designed the M80 website. During the development process, he provided design mockups for mobile, tablet, and desktop devices.</p>
+
+        <p>This website was built using WordPress to provide the client with the ability to manage their content. Included in the deliverable was a small custom library of classes that their in-house designers can leverage to modify the design of their website within branding guidelines.</p>
       </template>
       <template slot="side-content">
         <h3>Built With</h3>
@@ -13,16 +17,16 @@
           <p class="is-col-5" :style="$store.getters.darkBkgColor">Javascript</p>
           <p class="is-col-4" :style="$store.getters.darkBkgColor">PHP</p>
           <p class="is-col-8" :style="$store.getters.darkBkgColor">SCSS</p>
-          <p class="is-col-12 large" :style="$store.getters.darkBkgColor">Wordpress</p>
+          <p class="is-col-12 large" :style="$store.getters.darkBkgColor">WordPress</p>
         </div>
         <h3>My Role</h3>
-        <p>I was the <strong>tech lead</strong> and <strong>lead front-end developer</strong> on this project. In this role, I was
-          responsible for weekly deployments and the development of the company website. As the tech lead, I
-          managed a small group of front-end developers, delegated their tasks, and performed bi-weekly code
-          reviews to maintain a high coding standard.</p>
+        <p>I was the <strong>developer</strong> and <strong>technical consultant</strong> on this project. It was my job as a freelance consultant
+          to
+           provide the best possible solution for their business within their budget. I collaborated heavily with
+          their in-house designer to produce a product that reflected their brand as close as possible.</p>
 
-        <p>This project was built with Nuxt.js, SCSS, TypeScript, and more. External TypeScript utility functions
-          were tested using Jest with 90% code coverage.</p>
+        <p>This project was launched in the Summer of 2016. I continue to support this website for more significant
+          modifications like new anniversary pages.</p>
       </template>
       <template slot="testimonial">
         <theme-text class="is-bold" tag="h2">His Words Not Mine</theme-text>
