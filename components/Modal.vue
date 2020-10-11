29f1ad70dab588de1079c14c3d8c954a8dc52a52
Dave Berning <david@oodle.io>
2020-10-11 13:57:57 -0400

    user experience improvements

--- components/Modal.vue
+++ components/Modal.vue
@@ -2,158 +2,180 @@
   <div class="modal" :class="[ { active: isActive }, { regular: !this.isInverted },  { inverted: this.isInverted }]"
        :style="`background: ${backgroundColor}; color: ${textColor} !important;`">
     <div class="content" :class="{ regular: !this.isInverted }">
-      <nuxt-link to="/" class="close">
+      <a @click="getRoute()" class="close">
         <close-icon :use-theme-color="true" />
-      </nuxt-link>
+      </a>
       <slot />
     </div>
-    <p class="right" :style="`color: ${getThemeColors($store.state.theme).backgroundDarker}`">Copyright &copy {{ new Date().getFullYear() }}. All Rights Reserved.</p>
+    <p class="right" :style="`color: ${getThemeColors($store.state.theme).backgroundDarker}`">Copyright &copy
+      {{ new Date().getFullYear() }}. All Rights Reserved.</p>
   </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import CloseIcon from '~/components/CloseIcon.vue'
-  import { getThemeColors } from '@/data/theme-colors'
+import Vue from 'vue'
+import CloseIcon from '~/components/CloseIcon.vue'
+import { getThemeColors } from '@/data/theme-colors'
 
-  export default Vue.extend({
-    name: 'Modal' as string,
-    components: {
-      CloseIcon
-    },
-    props: {
-      isInverted: {
-        type: Boolean as () => boolean,
-        required: false,
-        default: false
-      }
-    },
-    data() {
-      return {
-        isActive: true as boolean,
-        bodyClass: 'modal-background' as string
-      }
-    },
-    destroyed(): void {
-      // @ts-ignore
-      this.isActive = false
-    },
-    methods: {
-      getThemeColors
-    },
-    computed: {
-      backgroundColor() {
-        return this.isInverted ? 'white' : `${getThemeColors(this.$store.state.theme).background}`
-      },
-      textColor() {
-        return this.isInverted ? `${getThemeColors(this.$store.state.theme).background}` : 'white'
-      }
-    },
-    mounted(): void {
-      // @ts-ignore
-      this.isActive = true
+export default Vue.extend({
+  name: 'Modal' as string,
+  components: {
+    CloseIcon
+  },
+  props: {
+    isInverted: {
+      type: Boolean as () => boolean,
+      required: false,
+      default: false
     }
-  })
+  },
+  data() {
+    return {
+      isActive: true as boolean,
+      bodyClass: 'modal-background' as string
+    }
+  },
+  destroyed(): void {
+    // @ts-ignore
+    this.isActive = false
+  },
+  methods: {
+    getThemeColors,
+    getRoute() {
+      if (process.client) {
+        const workPageRegex = new RegExp(/\/work\/(.+)/g)
+        const isProjectPage = workPageRegex.test(window.location)
+        if (isProjectPage) {
+          this.$router.push({ path: '/work' })
+        } else {
+          this.$router.push({ path: '/' })
+        }
+      }
+    }
+  },
+  computed: {
+    backgroundColor() {
+      return this.isInverted ? 'white' : `${getThemeColors(this.$store.state.theme).background}`
+    },
+    textColor() {
+      return this.isInverted ? `${getThemeColors(this.$store.state.theme).background}` : 'white'
+    }
+  },
+  mounted(): void {
+    // @ts-ignore
+    this.isActive = true
+  }
+})
 </script>
 
 <style lang="scss">
-  .modal {
-    height: 100vh;
-    width: 100vw;
-    padding: 1rem;
-    overflow: auto;
-    z-index: 10000;
-    @media screen and (min-width: 768px) { padding: 2rem; }
-    @media screen and (min-width: 1200px) { padding: 2rem 4rem; }
+.modal {
+  height: 100vh;
+  width: 100vw;
+  padding: 1rem;
+  overflow: auto;
+  z-index: 10000;
+  @media screen and (min-width: 768px) {
+    padding: 2rem;
+  }
+  @media screen and (min-width: 1200px) {
+    padding: 2rem 4rem;
+  }
 
-    h1 {
-      font-size: 4rem;
-      line-height: 7rem;
+  h1 {
+    font-size: 4rem;
+    line-height: 7rem;
+
+    @media screen and (min-width: $lg) {
+      font-size: 7rem;
+      line-height: 11rem;
+    }
+  }
+
+  .content {
+    transition-delay: .35s;
+    padding: 0 0 2rem 0;
+  }
+
+  &.regular * {
+    color: #ffffff;
+    text-align: left;
+    position: relative;
+  }
+
+  * {
+    text-align: left;
+    position: relative;
+  }
+
+  .close {
+    background: none;
+    color: #ffffff;
+    position: absolute;
+    top: -5px;
+    right: -15px;
+    padding: .5rem 1rem;
+    z-index: 9999;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    @media screen and (min-width: $lg) {
+      top: 15px;
+      right: -1rem;
+    }
+
+    svg {
+      width: 25px;
+      height: 25px;
 
       @media screen and (min-width: $lg) {
-        font-size: 7rem;
-        line-height: 11rem;
-      }
-    }
-
-    .content {
-      transition-delay: .35s;
-      padding: 0 0 2rem 0;
-    }
-
-    &.regular * {
-      color: #ffffff;
-      text-align: left;
-      position: relative;
-    }
-
-    * {
-      text-align: left;
-      position: relative;
-    }
-
-    .close {
-      background: none;
-      color: #ffffff;
-      position: absolute;
-      top: -5px;
-      right: -15px;
-      padding: .5rem 1rem;
-      z-index: 9999;
-
-      @media screen and (min-width: $lg) {
-        top: 15px;
-        right: -1rem;
-      }
-
-      svg {
-        width: 25px;
-        height: 25px;
-
-        @media screen and (min-width: $lg) {
-          width: 50px;
-          height: 50px;
-        }
-      }
-    }
-
-    .regular .close svg { fill: var(--white); }
-
-    p {
-      font-size: 1.25rem;
-      line-height: 2.25rem;
-      font-weight: 300;
-      margin-bottom: 2rem;
-
-      @media screen and (min-width: $lg) {
-        font-size: 1.5rem;
-        line-height: 3rem;
-      }
-
-      em {
-        font-style: italic;
-        font-weight: 600;
-        letter-spacing: .25px;
-      }
-
-      &.right {
-        position: relative;
-        bottom: 0;
-        margin: 1rem 0 0 0;
-        text-align: center;
-        right: 0;
-        font-size: .75rem;
-        color: lighten(#3e9e91, 20%);
-        line-height: 1rem;
-
-        @media screen and (min-width: $lg) {
-          position: absolute;
-          line-height: 3rem;
-          right: 4rem;
-          margin-top: 0;
-          text-align: unset;
-        }
+        width: 50px;
+        height: 50px;
       }
     }
   }
+
+  .regular .close svg {
+    fill: var(--white);
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 2.25rem;
+    font-weight: 300;
+    margin-bottom: 2rem;
+
+    @media screen and (min-width: $lg) {
+      font-size: 1.5rem;
+      line-height: 3rem;
+    }
+
+    em {
+      font-style: italic;
+      font-weight: 600;
+      letter-spacing: .25px;
+    }
+
+    &.right {
+      position: relative;
+      bottom: 0;
+      margin: 1rem 0 0 0;
+      text-align: center;
+      right: 0;
+      font-size: .75rem;
+      color: lighten(#3e9e91, 20%);
+      line-height: 1rem;
+
+      @media screen and (min-width: $lg) {
+        position: absolute;
+        line-height: 3rem;
+        right: 4rem;
+        margin-top: 0;
+        text-align: unset;
+      }
+    }
+  }
+}
 </style>
--- components/WorkContent.vue
+++ components/WorkContent.vue
@@ -2,10 +2,14 @@
   <theme-text>
     <div class="is-grid work">
       <theme-text tag="h1" class="is-col-7-md is-col-8-xl">{{ title }}</theme-text>
-      <div class="is-col-5-md is-col-4-xl link">
-        <a :href="url" target="_blank"
-           :style="`${$store.getters.darkTextColor}; ${$store.getters.darkBorderColor}`">{{ urlText }}</a>
-        <nuxt-link to="/work" :style="`${$store.getters.darkTextColor};`">View More Work</nuxt-link>
+      <div class="is-col-5-md is-col-4-xl link has-no-m">
+        <a
+          :href="url"
+          target="_blank"
+          :style="`${$store.getters.darkTextColor}; ${$store.getters.darkBorder}`"
+        >
+          {{ urlText }}
+        </a>
       </div>
     </div>
     <div class="is-grid">
@@ -29,134 +33,144 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from 'vue'
 
-  export default Vue.extend({
-    props: {
-      url: {
-        type: String as () => string,
-        required: false as boolean,
-        default: '' as string
-      },
-      urlText: {
-        type: String as () => string,
-        required: false as boolean,
-        default: 'Visit Website' as string
-      },
-      title: {
-        type: String as () => string,
-        required: true as boolean,
-      }
+export default Vue.extend({
+  props: {
+    url: {
+      type: String as () => string,
+      required: false as boolean,
+      default: '' as string
+    },
+    urlText: {
+      type: String as () => string,
+      required: false as boolean,
+      default: 'Visit Website' as string
+    },
+    title: {
+      type: String as () => string,
+      required: true as boolean
     }
-  })
+  }
+})
 </script>
 
 <style lang="scss">
-  .work h1 {
-    @media screen and (max-width: $md){
+.work h1 {
+  @media screen and (max-width: $md) {
+    text-align: center;
+  }
+
+  @media screen and (max-width: $xl) {
+    font-size: 3rem !important;
+    line-height: 7rem !important;
+  }
+}
+
+.tldr > div {
+  padding: 1rem 2rem 3rem 2rem;
+  border-radius: .25rem;
+
+  h3 {
+    font-weight: bold;
+    font-size: 3rem !important;
+    margin: 2rem 0;
+  }
+
+  & * {
+    color: #ffffff;
+    font-size: 1.25rem !important;
+    line-height: 2rem !important;
+  }
+}
+
+strong {
+  font-weight: bold !important;
+}
+
+.built-with p {
+  padding: .5rem 1rem;
+  border-radius: .25rem;
+  text-align: center;
+  margin: 0;
+  font-size: 1.5rem;
+  line-height: 3rem;
+}
+
+.link {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  margin-bottom: 2rem;
+
+  a {
+    border: 2px solid;
+    border-radius: 100px;
+    padding: 1rem 1rem;
+    text-decoration: none;
+    min-width: 15rem;
+    text-align: center;
+
+    &:last-of-type {
+      border: none;
+    }
+  }
+}
+
+img {
+  max-width: 80%;
+  display: block;
+  margin: 0 auto 2rem auto;
+}
+
+.testimonial {
+  max-width: 60rem;
+  margin: 4rem auto 0 auto;
+
+  h2 {
+    text-align: center;
+  }
+
+  p {
+    margin-top: 2rem;
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 2rem;
+
+    &:last-child {
+      margin-top: 1rem;
       text-align: center;
     }
-
-    @media screen and (max-width: $xl) {
-      font-size: 3rem !important;
-      line-height: 7rem !important;
-    }
   }
 
-  .tldr > div {
-    padding: 1rem 2rem 3rem 2rem;
-    border-radius: .25rem;
-
-    h3 {
-      font-weight: bold;
-      font-size: 3rem !important;
-      margin: 2rem 0;
-    }
-
-    & * {
-      color: #ffffff;
-      font-size: 1.25rem !important;
-      line-height: 2rem !important;
-    }
+  img:not([src*="svg"]) {
+    border-radius: 50%;
+    box-shadow: 0 .5em 1em -.125em rgba(10, 10, 10, .1), 0 0 0 1px rgba(10, 10, 10, .02);
   }
 
-  strong { font-weight: bold !important; }
-
-  .built-with p {
-    padding: .5rem 1rem;
-    border-radius: .25rem;
-    text-align: center;
-    margin: 0;
-    font-size: 1.5rem;
-    line-height: 3rem;
-  }
-
-  .link {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 2rem;
-
-    a {
-      border: 2px solid;
-      border-radius: 100px;
-      padding: 1rem 1rem;
-      text-decoration: none;
-
-      &:last-of-type {
-        border: none;
-      }
-    }
-  }
-
-  img {
-    max-width: 80%;
+  button {
+    border: 0;
+    padding: 1rem 3rem;
+    font-size: 1rem;
+    color: #ffffff;
     display: block;
-    margin: 0 auto 2rem auto;
-  }
+    margin: 0 auto;
+    border-radius: 100px;
 
-  .testimonial {
-    max-width: 60rem;
-    margin: 4rem auto 0 auto;
-
-    h2 { text-align: center; }
-
-    p { margin-top: 2rem; }
-
-    p {
-      font-size: 1.25rem;
-      line-height: 2rem;
-
-      &:last-child {
-        margin-top: 1rem;
-        text-align: center;
-      }
-    }
-
-    img:not([src*="svg"]) {
-      border-radius: 50%;
-      box-shadow: 0 .5em 1em -.125em rgba(10,10,10,.1),0 0 0 1px rgba(10,10,10,.02);
-    }
-
-    button {
-      border: 0;
-      padding: 1rem 3rem;
-      font-size: 1rem;
-      color: #fff;
-      display: block;
-      margin: 0 auto;
-      border-radius: 100px;
-
-      &:hover { cursor: pointer; }
-    }
-
-    a {
-      border: 2px solid;
-      border-radius: 100px;
-      padding: 1rem 1rem;
-      text-decoration: none;
-      display: inline-block;
-      margin: 0 auto;
+    &:hover {
+      cursor: pointer;
     }
   }
+
+  a {
+    border: 2px solid;
+    border-radius: 100px;
+    padding: 1rem 1rem;
+    text-decoration: none;
+    display: inline-block;
+    margin: 0 auto;
+  }
+}
 </style>
