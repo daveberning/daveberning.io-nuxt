53deb0f3b6f79d6d93cb75b813780ce13a541f6c
Dave Berning <daveberning@outlook.com>
2021-02-25 17:01:23 -0500

    new job!

--- data/writing.ts
+++ data/writing.ts
@@ -16,6 +16,13 @@ export const writing: Writing[] = [
     to: `https://www.amazon.com/Creating-Interfaces-Bulma-Jeremy-Thomas-ebook/dp/B079M1BJG4/ref=sr_1_1?keywords=creating+interfaces+with+bulma&qid=1566873504&s=books&sr=1-1`
   },
   {
+    title: `How To Use Vue.js Environment Modes with a Node.js Mock Data Layer`,
+    date: `February 10, 2021`,
+    featured: false,
+    type: `Digital Ocean`,
+    to: `https://www.digitalocean.com/community/tutorials/how-to-use-vue-js-environment-modes-with-a-node-js-mock-data-layer`
+  },
+  {
     title: `How To Generate a Vue.js Single Page App With the Vue CLI`,
     date: `January 11, 2021`,
     featured: false,
